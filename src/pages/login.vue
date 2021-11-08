d7fc91d7bb74a103f0df77071498c6152d4168f1
Brandon <blopez98@live.com>
2021-11-08 13:44:28 -0600

    Added Validation to form

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,13 +1,28 @@
 <script setup>
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+
+import {useForm, useField} from 'vee-validate';
+import * as yup from 'yup';
+
+const schema = yup.object({
+      username: yup.string().required().email(),
+      password: yup.string().required().min(8),
+    });
+
+useForm({
+      validationSchema: schema,
+    });
+
+const { value: username, errorMessage: emailError } = useField('username');
+const { value: password, errorMessage: passwordError } = useField('password');
+
+
+
 import useAuth from "../composable/useAuth";
 
 
 const { isAuthenticated, login, signup, googleLogin } = useAuth();
-
-const username = ref("");
-const password = ref("");
 
 const router = useRouter();
 
@@ -44,8 +59,10 @@ const goToHome = () =>
 <div class="flex flex-col justify-center text-center py-20 min-h-screen-nonav">
     <img class=" w-40 h-40 shadow-lg" src="/src/assets/login3.png" alt="Login Image"/>
     <form @submit.prevent="loggingIn" class=" bg-indigo-300 flex flex-col space-y-10">
-        <input type="text" class="rounded-lg border-2" placeholder="Username" v-model="username"/>
-        <input type="password" class="rounded-lg border-2 " placeholder="Password" v-model="password"/>
+        <input name="username" type="text" class="rounded-lg border-2" placeholder="Email" v-model="username"/>
+        <span class="text-pink-900 text-xl text-center">{{emailError}}</span>
+        <input name="password" type="password" class="rounded-lg border-2 " placeholder="Password" v-model="password"/>
+        <span class="text-pink-900 text-xl text-center">{{passwordError}}</span>
         <div class="flex justify-center space-x-3">
             <button type="submit" @submit.prevent="loggingIn" class="py-2 px-4 rounded-lg bg-blue-600 text-blue-200">Login</button>
             <button @click="signingUp" class=" py-2 px-4 rounded-lg bg-purple-600 text-red-200">Sign Up</button>

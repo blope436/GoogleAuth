9a2df673e08a4f88c0b36354779bd899236d5beb
Brandon <blopez98@live.com>
2021-11-29 12:18:31 -0600

    Made changes- Read from Firestore

--- src/composable/useFirebase.js
+++ src/composable/useFirebase.js
@@ -2,9 +2,12 @@ import { initializeApp } from "firebase/app";
 
 import {getAuth} from "firebase/auth"
 
+import { getFirestore } from "firebase/firestore";
+
 import { firebaseConfig } from "../firebaseConfig";
 
 export const firebaseapp = initializeApp(firebaseConfig);
 
 export const firebaseAuth = getAuth(firebaseapp);
 
+export const db = getFirestore();
--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,3 +1,16 @@
+<script setup>
+import { onUnmounted, ref } from 'vue';
+import useChat from '../composable/useChat'
+
+const {messages, unsubscribe} = useChat();
+
+const newMessage = ref("");
+
+onUnmounted(() => {
+    unsubscribe()
+})
+</script>
+
 <template>
 <div class="text-center py-7 text-indigo-500 text-5xl">
 <h2>This is a chat page that only authenticated users are allowed to use.</h2>
@@ -6,15 +19,15 @@
 <h1 class="text-indigo-500 text-8xl text-center">OnlineMarketPlace Chat</h1>
 <div class="flex flex-col min-h-[900px] w-full mt-6 rounded-lg justify-between border-4 border-red-600">
     <ul class="p-5 space-y-5">
-        <li v-for="n in 5" class="text-blue-600 text-4xl">
+        <li v-for="message in messages" :key="message.id" class="text-blue-600 text-4xl">
             <div class="flex justify-between bg-green-200 px-5 py-3 rounded-2xl">
-                <span>Message: </span>
-                <span>Author Name:</span>
+                <span>Message: {{message.text}} </span>
+                <span>Author Name: {{message.author}}</span>
             </div>
         </li>
     </ul>
     <div>
-        <input class="w-full min-h-[100px] p-5 rounded-xl focus:outline-none focus:bg-pink-300" type="text" placeholder="Type a message here"/>
+        <input class="w-full min-h-[100px] p-5 rounded-xl focus:outline-none focus:bg-pink-300" type="text" placeholder="Type a message here" v-model="newMessage"/>
     </div>
 </div>
 </template>
